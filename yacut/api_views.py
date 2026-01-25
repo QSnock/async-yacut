c61bba43b448f85178acc149e7a17029d3c097b4
Евгений Сульжицкий <idholleyyt@vk.com>
2026-01-25 18:07:20 +0300

    Исправления линтера

--- yacut/api_views.py
+++ yacut/api_views.py
@@ -17,26 +17,27 @@ def validate_custom_id(custom_id):
     return True
 
 
-def _save_to_db(url, short_id):
-    """Сохраняет URLMap в базу данных."""
+def save_url_map(url, short_id):
+    """Сохраняет URLMap в бд."""
     url_map = URLMap(original=url, short=short_id)
     db.session.add(url_map)
     db.session.commit()
 
 
-@app.route('/api/id/', methods=['POST'])
-def create_id():
-    """Создание короткой ссылки через API."""
+def validate_request():
+    """Проверяет запрос и извлекает данные."""
     if not request.is_json:
-        return jsonify({'message': 'Отсутствует тело запроса'}), 400
+        return None, jsonify({'message': 'Отсутствует тело запроса'}), 400
 
     data = request.get_json(silent=True)
     if data is None:
-        return jsonify({'message': 'Отсутствует тело запроса'}), 400
+        return None, jsonify({'message': 'Отсутствует тело запроса'}), 400
 
     url = data.get('url')
     if not url:
-        return jsonify({'message': '"url" является обязательным полем!'}), 400
+        return None, jsonify(
+            {'message': '"url" является обязательным полем!'}
+        ), 400
 
     custom_id = data.get('custom_id')
     if custom_id:
@@ -44,32 +45,57 @@ def create_id():
     if not custom_id:
         custom_id = None
 
-    if custom_id:
-        if not validate_custom_id(custom_id):
-            return jsonify(
-                {'message': 'Указано недопустимое имя для короткой ссылки'}
-            ), 400
+    return (url, custom_id), None, None
 
-        existing = URLMap.query.filter_by(short=custom_id).first()
-        if existing:
-            return jsonify(
-                {'message': 'Предложенный вариант короткой ссылки уже существует.'}
-            ), 400
 
-        short_id = custom_id
-    else:
-        short_id = get_unique_short_id()
+def get_short_id(custom_id):
+    """Получает short_id, проверяя custom_id если он указан."""
+    if not custom_id:
+        return get_unique_short_id(), None, None
+
+    if not validate_custom_id(custom_id):
+        return None, jsonify(
+            {'message': 'Указано недопустимое имя для короткой ссылки'}
+        ), 400
+
+    existing = URLMap.query.filter_by(short=custom_id).first()
+    if existing:
+        return None, jsonify(
+            {
+                'message': 'Предложенный вариант короткой '
+                           'ссылки уже существует.'
+            }
+        ), 400
+
+    return custom_id, None, None
+
+
+@app.route('/api/id/', methods=['POST'])
+def create_id():
+    """Создание короткой ссылки через API."""
+    request_data, error_response, error_code = validate_request()
+    if error_response:
+        return error_response, error_code
+
+    url, custom_id = request_data
+
+    short_id, error_response, error_code = get_short_id(custom_id)
+    if error_response:
+        return error_response, error_code
 
     try:
-        _save_to_db(url, short_id)
+        save_url_map(url, short_id)
     except Exception:
         db.session.rollback()
         if custom_id:
             return jsonify(
-                {'message': 'Предложенный вариант короткой ссылки уже существует.'}
+                {
+                    'message': 'Предложенный вариант короткой '
+                               'ссылки уже существует.'
+                }
             ), 400
         short_id = get_unique_short_id()
-        _save_to_db(url, short_id)
+        save_url_map(url, short_id)
 
     short_link = url_for('redirect_view', short_id=short_id, _external=True)
     return jsonify({
